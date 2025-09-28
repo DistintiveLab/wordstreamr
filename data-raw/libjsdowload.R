e28bb035fcbc51cc11419a2839e433b1d106d3e2
Rodrigo E. S. Borges <borges@distintive.com.br>
2025-09-28 15:17:46 +0000

    added github hooks

--- data-raw/libjsdowload.R
+++ data-raw/libjsdowload.R
@@ -3,7 +3,8 @@
 download.file("https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js",
 "inst/htmlwidgets/lib/d3.min.js")
 
-download.file("https://github.com/huyen-nguyen/wordstream-library/raw/refs/heads/master/js/wordstream.js",
+##added as module
+#download.file("https://github.com/huyen-nguyen/wordstream-library/raw/refs/heads/master/js/wordstream.js",
               "inst/htmlwidgets/lib/wordstream.js")
 
 #usethis::use_data(d3minjs, overwrite = TRUE)
